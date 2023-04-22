308bf1d94127b2e35bb0cfc7e3bbe2b2e3d5d89a
kabute muthoni <kabutemuthoni@gmail.com>
2023-04-22 10:23:16 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef HEADER
+#define HEADER
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+
+int _print_chr(char q);
+int _printf(const char *format, ...);
+int _print_str(char *str);
+
+#endif
