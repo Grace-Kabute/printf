ffd8706618c79b0876be7b53fdc05bfbc2b9b0f5
AlexRugara <alexrugara1@gmail.com>
2023-04-24 12:47:05 +0300

    Handled th b precision

--- our_printf.c
+++ our_printf.c
@@ -196,4 +196,48 @@ int _print_number(va_list typez, char *buffer, int flags, int width, int precisi
 	i++;
 	return (write_number(a_negative, i, buffer, flags, width, precision, size));
 }
+/**
+ * _print_binary - print out a number in binary
+ * @typez: list a of arguments
+ * @buffer: Buffer to handle print output
+ * @flags: identify active flags
+ * @width: gives width of the character
+ * @precision: identify precisions
+ * @size: specify the size of the character
+ * Return: binary number of character printed
+ */
+int _print_binary(va_list typez, char *buffer, int flags, int width, int precision, int size)
+{
 
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int counter;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+
+	n = va_arg(typez, unsigned int);
+	n = 2147483648;
+
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		 m = m / 2;
+		 a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, counnter = 0; i < 32; i++)
+	{
+		sum = sum + a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+			write(1, &z, 1);
+			counter++;
+		}
+	}
+	return (counter);
+}
