dd36e259ff14903a034b8a154171d66a9ebab00a
AlexRugara <alexrugara1@gmail.com>
2023-04-24 10:42:27 +0300

    We added a function to print percentage

---
+++ main.h
@@ -0,0 +1,37 @@
+#ifndef HEADER
+#define HEADER
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+
+#define BUFFFER_SIZE 1024
+
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_space 16
+int _print_chr(char q);
+int _print_char(va_list typez, char *buffer, int flags, int width, int precision, int size);
+int _printf(const char *format, ...);
+void print_buffer(char *buffer, int *buffer_index);
+int _print_str(va_list typez, char *buffer, int flags, int width, int precision, int size);
+int _print_percent(va_list typez, char *buffer, int flags, int width, int precision, int size);
+int _print_number(va_list typez, char *buffer, int flags, int width, int precision, int size);
+int _print_binary(va_list typez, char *buffer, int flags, int width, int precision, int size);
+int _print_unsigned_num(va_list typez, char *buffer, int flags, int width, int precision, int size);
+int _print_octal(va_list typez, char *buffer, int flags, int width, int precision, int size);
+int _print_hexa_decimals(va_list typez, char *buffer, int flags, int width, int precision, int size);
+int _print_hexa_capital(va_list typez, char *buffer, int flags, int width, int precision, int size);
+int _print_handler(va_list typez, char *buffer, int flags, int width, int precision, int size);
+int handle_write_chr(va_list typez, char *buffer, int flags, int width, int precision, int size);
+int handle_number(va_list typez, char *buffer, int flags, int width, int precision, int size);
+int write_number(va_list typez, char *buffer, int flags, int width, int precision, int size);
+int write_pointer(va_list typez, char *buffer, int flags, int width, int precision, int size);
+int unsigned_number(va_list typez, char *buffer, int flags, int width, int precision, int size);
+
+
+
+#endif /*HEADER*/
---
+++ our_printf.c
@@ -0,0 +1,159 @@
+#include "main.h"
+
+/**
+ * _printf - produces output as written
+ * @format: string passed in function
+ * Return: return length of string
+ */
+int _printf(const char *format, ...)
+{
+	int i, counter, output = 0;
+	int flagz, width, precision, size, buffer_index = 0;
+	char buffer[BUFFER_SIZE];
+
+	va_list list;
+
+	va_start(list, format);
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	counter = 0;
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buffer_index++] = format[i];
+			if (buffer_index == BUFFER_SIZE)
+				print_buffer(buffer, &buffer_index);
+			counter++;
+		}
+		else
+		{
+			print_buffer(buffer, &buffer_index);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i);
+			size = get_size(format, &i);
+			++i;
+			output = handle_print(format, &i, list, buffer, flags, width, precision, size);
+			if (output == -1)
+				return (-1);
+			counter += output;
+		}
+	}
+	print_buffer(buffer, &buffer_index);
+	va_end(list);
+	return (counter);
+}
+/**
+ * print_buffer - Prints the content of the buffer if it available
+ * @buffer: pointer to string
+ * @buffer_index: index to store curret character format[i]
+ */
+ void print_buffer(char *buffer, *buffer_index)
+{
+	if (*buffer_index > 0)
+		write(1, &buffer, *buffer_index);
+	*buffer_index = 0;
+}
+/**
+ * _print_chr - prints out one character
+ * @q: input character
+ * Return: return 1
+ */
+int _print_chr(char q)
+{
+	write(1, &q, 1);
+	return (1);
+}
+/**
+* _print_char - Prints a character
+* @types: List a of arguments passed
+* @buffer: Buffer array to handle our output
+* @flags: identifies the active flags
+* @width: Width of character
+* @precision: specifies precision
+* @size: specifies the size
+* Return: Number of characters passed
+*/
+int _print_char(va_list typez, char buffer[],int flags, int width, int precision, int size)
+{
+	char q = va_arg(typez, int);
+	return (handle_write_char(c, buffer, flags, width, precision, size));
+}
+/**
+* _print_str - prints out a string
+* @types: List a of arguments passed
+* @buffer: Buffer array to handle our output
+* @flags: identifies the active flags
+* @width: Width of character
+* @precision: specifies precision
+* @size: specifies the size
+* Return: lenght of string
+ */
+int _print_str(va_list typez, char buffer[],int flags, int width, int precision, int size)
+{
+	int len, counter;
+	char *string = va_arg(typez, char *);
+	
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNSED(precision);
+	UNUSED(size);
+	if(str == NULL)
+	{
+		string = "(null)";
+		if (precision >= 6)
+			string =" ";
+	}
+	while (string[len] != '\0')
+		len++;
+	if (precision >= 0 && precison < len)
+		len = precision;
+	if (width > len )
+	{
+		if (flag & F_MINUS)
+		{
+			write(1, &string, len;
+			for (i = width -len; i > 0; i--)
+			write(1,"", 1);
+			
+			return (width);
+		}
+		else
+		{
+		for (i = width -len; i > 0; i--)
+			write(1,"",1);
+			write(1, &string, len);
+			return (width);
+		}
+	}
+			return (write(1, string, len));
+}
+
+/**
+  * _print_percent - prints the % sign
+  * @types: List a of arguments passed
+  * @buffer: Buffer array to handle our output
+  * @flags: identifies the active flags
+  * @width: Width of characte
+  * @precision: specifies precision
+  * @size: specifies the size
+  * Return: Return number of chracters output
+  */
+int _print_percent(va_list typez, char *buffer, int flags, int width, int precision, int size)
+{
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(typez);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	return (write(1, "%%", 1));
+}
+
